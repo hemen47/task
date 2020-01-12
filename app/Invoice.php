cfe0ffd98544bcc38d413a338950616468ebeaec
Hemen Badri <hemen_badri@yahoo.com>
2020-01-12 12:33:06 +0330

    Signed-off-by: Hemen Badri <hemen_badri@yahoo.com>

---
+++ app/Invoice.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    //
+    protected $guarded = [];
+
+    public function receiver(){
+        return $this->belongsTo('App\Receiver');
+    }
+}
